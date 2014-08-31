898b50a2abe3a5a8be285e8a9a70e792ad56cf48
Rodrigo G. de A <rodrigo.gdea@gmail.com>
2014-08-31 18:19:04 -0300

    Adding ReactiveMongo dependency.

--- scala/build.sbt
+++ scala/build.sbt
@@ -4,5 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.1"
 
-libraryDependencies +=
-  "org.specs2" %% "specs2" % "2.4.1" % "test"
+resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
+libraryDependencies ++= Seq(
+  "org.specs2" %% "specs2" % "2.4.1" % "test",
+  "org.reactivemongo" %% "reactivemongo" % "0.10.5.akka23-SNAPSHOT"
+)
--- scala/src/test/scala/SimpleMongoMapperSpec.scala
+++ scala/src/test/scala/SimpleMongoMapperSpec.scala
@@ -1,4 +1,15 @@
+import java.util.concurrent.TimeUnit
+
+import akka.actor.FSM.Failure
 import org.specs2.mutable.Specification
+import reactivemongo.api._
+import reactivemongo.api.collections.default.BSONCollection
+import reactivemongo.bson.BSONDocument
+import reactivemongo.core.commands.LastError
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Future, Await}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
 
 /**
  * Created by rodrigo.gdea@gmail.com on 28/08/14.
@@ -6,6 +17,15 @@ import org.specs2.mutable.Specification
 class SimpleMongoMapperSpec extends Specification {
   "The 'Hello world' string" should {
     "contain 11 characters" in {
+      val driver: MongoDriver = new MongoDriver
+      val connection: MongoConnection = driver.connection(List("localhost"))
+      val db: DefaultDB = connection("simple-mongo-mapper")
+      val collection: BSONCollection = db("test")
+      val insert: Future[LastError] = collection.insert(BSONDocument("firstName" -> "Test"))
+      insert.onComplete( x => println(s"${x}\n"))
+
+      Await.ready(insert, Duration(2, TimeUnit.SECONDS))
+
       "Hello world" must have size (11)
     }
     "start with 'Hello'" in {
